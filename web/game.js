93c22b3522c6d40b841629bdcdbf89dcb37e9de6
Robert McDermot <robert.mcdermot@gmail.com>
2014-02-14 12:43:52 -0500

    debug

--- web/game.js
+++ web/game.js
@@ -3555,8 +3555,8 @@ var $$ = {};
     if (intvalue != null) {
       volumeSlider = H.interceptedTypeCast(document.querySelector("#VolumeSlider"), "$isInputElement");
       $.get$localStorage().setItem("prevVolume", volumeSlider.value);
-      volumeSlider.value = J.trim$0$s(value);
-      H.interceptedTypeCast(document.querySelector("#rangevalue"), "$isOutputElement").value = value;
+      volumeSlider.value = J.getInterceptor$s(value).trim$0(value);
+      document.querySelector("#rangevalue").textContent = C.JSString_methods.trim$0(value);
       t1 = $.get$ui().currentSong;
       if (t1 != null)
         J.volume$1$x(t1, intvalue);
@@ -11418,7 +11418,6 @@ var $$ = {};
   },
   OutputElement: {
     "": "HtmlElement;name%,type=,value%",
-    $isOutputElement: true,
     "%": "HTMLOutputElement"
   },
   ParamElement: {
